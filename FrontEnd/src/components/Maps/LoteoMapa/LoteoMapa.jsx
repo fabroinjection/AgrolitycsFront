a456c067f9ea9ff7080abbc8c4c363b278466578
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2024-03-01 11:47:00 -0300

    correcciones código

--- FrontEnd/src/components/Maps/LoteoMapa/LoteoMapa.jsx
+++ FrontEnd/src/components/Maps/LoteoMapa/LoteoMapa.jsx
@@ -33,14 +33,6 @@ function Mapa({ habilitado = false, registro = false, campo, lotes }){
   const [ loteSeleccionadoAConsultar ] = useState(Cookies.get("idLoteSeleccionadoAConsultar"));
   const [ idLoteSeleccionadoAModificar ] = useState(Cookies.get("idLoteAModificar"));
 
-  const marquerIcon = new L.Icon({
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
   const [ data, loading, error ] = useWeather(campo.localidad_centroide_lat, campo.localidad_centroide_lon);
 
   const mapRef = useRef(null);
