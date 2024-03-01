85b6b386215a81f75c69f4745a5e36f7e9059a59
fabroinjection <84641558+fabroinjection@users.noreply.github.com>
2024-02-29 21:50:33 -0300

    otra prueba

--- FrontEnd/src/services/login.service.jsx
+++ FrontEnd/src/services/login.service.jsx
@@ -4,7 +4,7 @@ import { URLAPI } from "../config";
 
 export const loginService = async credentials => {
 
-    const _urlApi = `${URLAPI}token/`;
+    const _urlApi = `${URLAPI}/token/`;
     const response = await axios.post(_urlApi, credentials);
 
     return response;
